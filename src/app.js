036814eeb617fa256d8b1032867f232552a1bfac
Francisco Gargiulo <francisco_gargiulo@hotmail.com>
2023-02-28 20:35:12 -0300

    Added comments on code

--- src/app.js
+++ src/app.js
@@ -2,35 +2,44 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
-
 const session = require("express-session");
-
 const RedisStore = require("connect-redis")(session);
 const Redis = require("ioredis");
 
+// Create an Express app
 const app = express();
 
+// Configure session middleware
 app.use(
   session({
-    store: new RedisStore({ client: new Redis() }),
-    secret: "s3cr3t.",
-    name: "sid",
-    resave: false,
-    saveUninitialized: true,
+    store: new RedisStore({ client: new Redis() }), // Use Redis to store session data
+    secret: "s3cr3t.", // A secret key used to sign the session ID cookie
+    name: "sid", // Name of the session ID cookie
+    resave: false, // Do not save the session if it was not modified
+    saveUninitialized: true, // Save uninitialized sessions (i.e., new and not modified)
   })
 );
 
+// Configure the app's views directory and view engine
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+// Add middleware to log requests in the console
 app.use(logger("dev"));
+
+// Parse incoming requests with JSON payloads
 app.use(express.json());
+
+// Parse incoming requests with urlencoded payloads
 app.use(express.urlencoded({ extended: false }));
+
+// Parse cookies sent with the request
 app.use(cookieParser());
 
+// Route all requests to the router module
 app.use("/", require("./router"));
 
-// error handler
+// Define an error handling middleware function that redirects to a specified URL with an error message
 app.use(function (
   { status, code, description, redirect_uri, state },
   _,
@@ -38,25 +47,30 @@ app.use(function (
   next
 ) {
   if (!code) {
+    // If no error code is provided, respond with a generic server error
     return res.status(500).json({
       code: "server_error",
       description: "Internal Server Error",
     });
   }
 
-  const urlRedirect = new URL(redirect_uri);
+  const urlRedirect = new URL(redirect_uri); // Create a new URL object for the redirect URL
 
-  urlRedirect.searchParams.set("error", code);
+  urlRedirect.searchParams.set("error", code); // Set the "error" query parameter to the error code
 
   if (description) {
+    // If an error description is provided, set the "error_description" query parameter
     urlRedirect.searchParams.set("error_description", description);
   }
 
   if (state) {
+    // If a state parameter is provided, set the "state" query parameter
     urlRedirect.searchParams.set("state", state);
   }
 
+  // Redirect to the URL with the error information in the query string
   res.redirect(urlRedirect);
 });
 
+// Export the Express app object for use in other modules
 module.exports = app;
--- src/domain/authorization-code.js
+++ src/domain/authorization-code.js
@@ -1,20 +1,24 @@
+// Define a class called AuthorizationCode
 module.exports = class AuthorizationCode {
   constructor(sub, client_id, redirect_uri) {
-    this.client_id = client_id;
-    this.sub = sub;
-    this.redirect_uri = redirect_uri;
+    this.client_id = client_id; // Set the client ID property to the provided client ID
+    this.sub = sub; // Set the sub (subject) property to the provided sub
+    this.redirect_uri = redirect_uri; // Set the redirect URI property to the provided redirect URI
 
-    // A maximum authorization code lifetime of 10 minutes is RECOMMENDED.
+    // Set the authorization code's expiration time to 10 minutes from the current time
+    // This value is in ISO 8601 format and is stored as a string
+    // Note: the 10-minute expiration time is a recommendation, not a requirement
     this.expires_at = new Date(Date.now() + 6000).toISOString();
   }
 
+  // Define a toJSON method that returns an object representation of the AuthorizationCode instance
   toJSON = () => {
     return {
-      id: this.id,
-      sub: this.sub,
-      client_id: this.client_id,
-      expires_at: this.expires_at,
-      redirect_uri: this.redirect_uri,
+      id: this.id, // The authorization code ID is not included, as it is not set by the constructor
+      sub: this.sub, // Include the sub (subject) property
+      client_id: this.client_id, // Include the client ID property
+      expires_at: this.expires_at, // Include the expiration time property
+      redirect_uri: this.redirect_uri, // Include the redirect URI property
     };
   };
 };
